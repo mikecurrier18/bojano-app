377bf14d4cb674488a9ec6da801b99be950f379f
Michael Currier <mikecurrier18@gmail.com>
2024-08-16 17:12:37 -0400

    init

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    label: "Home",
+    route: "/",
+    icon: "/icons/home.svg",
+  },
+  {
+    label: "Gallery",
+    route: "/gallery",
+    icon: "/icons/gallery.svg",
+  },
+  {
+    label: "Create",
+    route: "/create",
+    icon: "/icons/create.svg",
+  },
+];
---
+++ types/index.d.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+
+// ====== USER PARAMS
+declare type CreateUserParams = {
+    clerkId: string;
+    email: string;
+    username: string;
+    firstName: string | null;
+    lastName: string | null;
+    photo: string;
+  };
+  
+  declare type UpdateUserParams = {
+    firstName: string | null;
+    lastName: string | null;
+    username: string;
+    photo: string;
+  };
+
+
+// ====== URL QUERY PARAMS
+declare type FormUrlQueryParams = {
+    searchParams: string;
+    key: string;
+    value: string | number | null;
+  };
+  
+  declare type UrlQueryParams = {
+    params: string;
+    key: string;
+    value: string | null;
+  };
+  
+  declare type RemoveUrlQueryParams = {
+    searchParams: string;
+    keysToRemove: string[];
+  };
+  
+  declare type SearchParamProps = {
+    params: { id: string; type: TransformationTypeKey };
+    searchParams: { [key: string]: string | string[] | undefined };
+  };
