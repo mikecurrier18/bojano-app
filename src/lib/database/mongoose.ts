0a12300bee44456d2e0d6c48fa8db5964652a1ee
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-09-22 12:41:53 -0400

    fix: make MONGODB_URL throw error if missing at definition instead of at usage

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -1,5 +1,14 @@
 import mongoose, { Mongoose } from "mongoose";
-const MONGODB_URL = process.env.MONGODB_URL;
+
+const MONGODB_URL = (() => {
+  const value = process.env.MONGODB_URL;
+
+  if (value === undefined) {
+    throw new Error("missing environment variable 'MONGODB_URL'");
+  }
+
+  return value;
+})();
 
 interface MongooseConnection {
   conn: Mongoose | null;
@@ -7,19 +16,22 @@ interface MongooseConnection {
 }
 
 let cached: MongooseConnection = (global as any).mongoose;
+
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
-  if (!MONGODB_URL) throw new Error("No MongoDB URL");
+  if (cached.conn) {
+    return cached.conn;
+  }
+
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
       bufferCommands: false,
     });
-    
+
   cached.conn = await cached.promise;
   return cached.conn;
 };
