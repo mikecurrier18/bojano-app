705124dba5568f4184a71aa9f4149e462cbff746
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-09-22 12:44:58 -0400

    refactor(api): move errors.ts to lib directory

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,13 +1,13 @@
 /**
  * Base exception for all Bojano-related errors.
  */
-class BojanoException extends Error {}
+export class BojanoException extends Error {}
 
 /**
  * An error thrown when the API receives a year greater than the current year,
  * or if the year is not in 4-digit YYYY format.
  */
-class InvalidYearError extends BojanoException {
+export class InvalidYearError extends BojanoException {
   constructor(message: string) {
     super(message);
     this.name = "InvalidYearError";
@@ -18,7 +18,7 @@ class InvalidYearError extends BojanoException {
  * An error thrown when the API receives a month that is outside of
  * the range [1...12], or if the month is not in 2-digit MM format.
  */
-class InvalidMonthError extends BojanoException {
+export class InvalidMonthError extends BojanoException {
   constructor(message: string) {
     super(message);
     this.name = "InvalidMonthError";
