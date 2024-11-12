4469327b218fbc534fb9d836cb0d46f1940cb3da
Nicolas Gonzalez <ndgfrs@gmail.com>
2024-11-12 11:44:07 -0500

    fix errors and implement a few more endpoints

--- api/src/bojano_api/mongodb.py
+++ api/src/bojano_api/mongodb.py
@@ -3,5 +3,4 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from .constants import MONGODB_URL
 
 client = AsyncIOMotorClient(MONGODB_URL)
-db_dev = client["LAB"]
-db_prod = client["BH"]
+db = client["LAB"]
